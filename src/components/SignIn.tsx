335922b25833df2b56c3f67fe483295ce497fe96
lucifiel0121 <zyi3204@gmail.com>
2022-12-23 05:14:24 +0800

    feat(function): add sign function

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,14 +1,83 @@
-import { checkDuringTime } from "../../utils/times";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useContextSelector } from "use-context-selector";
+import RootContext from "../../context/rootContext";
+import { TimeConfig } from "../../utils/times";
+import useLocalStorage, { SIGNIN_STORAGE_KEY } from "../hooks/useLocalStorage";
+
+import { useTime } from "../hooks/useTime";
 
 const SignIn = () => {
+  const { isLogin } = useContextSelector(RootContext, (v) => v[0]);
+  const [state, dispatch] = useTime();
+  const { sign, display } = state;
+  //@ts-ignore
+  const isSignIn = sign[display.key];
+  const [signState, setSignState] = useState(isSignIn);
+  const { saveSignInState } = useLocalStorage({ key: SIGNIN_STORAGE_KEY });
+
+  const handleClick = () => {
+    // check: has sign in
+    console.log("test");
+    if (isSignIn) {
+      return;
+    }
+    saveSignInState(state);
+    setSignState(true);
+    checkTimeDisplay();
+  };
+
+  //new Date("2022-12-23T15:24:43.391Z");
+  /**
+   * time cofig:
+   * value         |        08:00       11:00       19:00    23:00
+   * key           |    dayBegin   afternoonBegin  afternoonEnd   dayEnd
+   *               |-----------|-----------|------------|----------|-------->
+   * range         |  無效打卡   |   上午    |    中午     |    晚上   |  無效打卡
+   */
+  const checkTimeDisplay = (time = new Date()) => {
+    const hours = time.getHours();
+    if (hours < TimeConfig.dayBegin)
+      dispatch({ type: "invalid", time: time, signState: signState });
+    if (hours >= TimeConfig.dayBegin && hours < TimeConfig.afternoonBegin)
+      dispatch({ type: "morning", time: time, signState: signState });
+    if (hours >= TimeConfig.afternoonBegin && hours < TimeConfig.afternoonEnd)
+      dispatch({ type: "afternoon", time: time, signState: signState });
+    if (hours >= TimeConfig.afternoonEnd && hours < TimeConfig.dayEnd)
+      dispatch({ type: "evening", time: time, signState: signState });
+    if (hours >= TimeConfig.dayEnd)
+      dispatch({ type: "invalid", time: time, signState: signState });
+  };
+  useEffect(() => {
+    checkTimeDisplay();
+  }, []);
+
   return (
     <>
       <h1 className="text-5xl font-extrabold tracking-tight  sm:text-[5rem]">
-        打卡 <span className="text-[#7a66ff]">{checkDuringTime()}</span>
+        打卡狀態: <span className="text-[#7a66ff]">{state.display.value}</span>
       </h1>
-      <button className="rounded-xl bg-slate-700 p-4 text-white	hover:bg-slate-700/90">
-        簽到
-      </button>
+      {isLogin ? (
+        state.display.key === "invalid" || signState ? (
+          <button className="rounded-xl bg-slate-700/80 p-4 text-white">
+            不可簽到
+          </button>
+        ) : (
+          <button
+            onClick={handleClick}
+            className="rounded-xl bg-slate-700 p-4 text-white	hover:bg-slate-700/90"
+          >
+            簽到
+          </button>
+        )
+      ) : (
+        <Link
+          href="/login"
+          className="rounded-xl bg-slate-700 p-4 text-white	hover:bg-slate-700/90"
+        >
+          登入
+        </Link>
+      )}
     </>
   );
 };
--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -23,53 +23,55 @@ export enum TimeKey {
   "invalid",
 }
 
-export type State = {
-  sign: {
-    morning: boolean;
-    afternoon: boolean;
-    evening: boolean;
-  };
-  time: Date;
-  display: {
-    key: TimeKey;
-    value: Time;
-  };
-};
+export type State =
+  | {
+      sign: {
+        morning: boolean;
+        afternoon: boolean;
+        evening: boolean;
+      };
+      time: Date;
+      display: {
+        key: TimeKey;
+        value: Time;
+      };
+    }
+  | typeof initialState;
 
 type Action =
-  | { type: "morning"; payload: Date }
-  | { type: "afternoon"; payload: Date }
-  | { type: "evening"; payload: Date }
-  | { type: "invalid"; payload: Date };
+  | { type: "morning"; time: Date; signState: boolean }
+  | { type: "afternoon"; time: Date; signState: boolean }
+  | { type: "evening"; time: Date; signState: boolean }
+  | { type: "invalid"; time: Date; signState: boolean };
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case "morning":
       console.log("morning", state);
       return {
-        sign: { ...state.sign, morning: true },
-        time: action.payload,
+        sign: { ...state.sign, morning: action.signState },
+        time: action.time,
         display: { key: "morning", value: Time.morning },
       };
     case "afternoon":
       console.log("afternoon", state);
       return {
-        sign: { ...state.sign, afternoon: true },
-        time: action.payload,
+        sign: { ...state.sign, afternoon: action.signState },
+        time: action.time,
         display: { key: "afternoon", value: Time.afternoon },
       };
     case "evening":
       console.log("evening", state);
       return {
-        sign: { ...state.sign, evening: true },
-        time: action.payload,
+        sign: { ...state.sign, evening: action.signState },
+        time: action.time,
         display: { key: "evening", value: Time.evening },
       };
     case "invalid":
       console.log("invalid", state);
       return {
         sign: { ...state.sign },
-        time: action.payload,
+        time: action.time,
         display: { key: "invalid", value: Time.invalid },
       };
 
@@ -78,5 +80,4 @@ const reducer = (state: State, action: Action) => {
   }
 };
 
-// @ts-ignore
 export const useTime = () => useReducer(reducer, initialState);
